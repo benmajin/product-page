c1a24c1928d5cbe7c2b8b3ce3453a218accc4668
benmajin <benjamin.voissiere@hotmail.fr>
2022-11-18 13:49:41 +0100

    fixed type error

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-// eslint-disable-next-line import/no-unresolved
 import App from "./App"
 import "./input.css"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") as Element).render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>
